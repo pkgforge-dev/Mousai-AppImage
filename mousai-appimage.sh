6d1f8940d11ddfee236eb66afd41bd2d13c203b4
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-13 08:13:11 +0100

    There's no need to manually set gsettings backend anymore

--- mousai-appimage.sh
+++ mousai-appimage.sh
@@ -21,9 +21,6 @@ wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun /usr/bin/mousai /usr/lib/gio/modules/libgiognutls.so*
 
-## Set gsettings to save to keyfile, instead to dconf
-echo "GSETTINGS_BACKEND=keyfile" >> ./AppDir/.env
-
 # Make the AppImage with uruntime
 ./quick-sharun --make-appimage
 
