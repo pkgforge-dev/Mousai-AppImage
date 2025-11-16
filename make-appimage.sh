779b09c9aaad58d416bfbbcaaca8a81378737ebc
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:21:31 +0100

    Migrate to new generic AppImage template

---
+++ make-appimage.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+VERSION=$(pacman -Q mousai | awk '{print $2; exit}')
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.seadve.Mousai.svg
+export DESKTOP=/usr/share/applications/io.github.seadve.Mousai.desktop
+export DEPLOY_OPENGL=1
+export DEPLOY_PIPEWIRE=1
+export DEPLOY_GSTREAMER=1
+export STARTUPWMCLASS=mousai # For Wayland, this is 'io.github.seadve.Mousai', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
+
+# Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
+quick-sharun /usr/bin/mousai \
+             /usr/lib/gio/modules/libgiognutls.so*
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
